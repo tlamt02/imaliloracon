f181a4c074b68a5290d78bdba1131b791d8424e4
Drthnnn <thelam.than.1402@gmail.com>
2025-04-25 09:20:31 +0200

    update nrhck

--- Blogging/script.js
+++ Blogging/script.js
@@ -7,3 +7,17 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
         });
     });
 });
+
+function handleDragStart(e) {
+    this.style.opacity = '0.4';
+  }
+  
+  function handleDragEnd(e) {
+    this.style.opacity = '1';
+  }
+  
+  let items = document.querySelectorAll('.container .box');
+  items.forEach(function (item) {
+    item.addEventListener('dragstart', handleDragStart);
+    item.addEventListener('dragend', handleDragEnd);
+  });
